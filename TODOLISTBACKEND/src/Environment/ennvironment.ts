d781261049b57f6ab4f347785aa9f9f7bfc0b6b2
@MuthuvelMariappan <muthuvel.m@finstein.ai>
2024-08-05 12:34:17 +0530

    ENV file Added

--- TODOLISTBACKEND/src/Environment/ennvironment.ts
+++ TODOLISTBACKEND/src/Environment/ennvironment.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: true,
     firebase: {
-        apiKey: "AIzaSyAG3LBqWOTpnAXzbvbH-jZg_otfeAyZyv4",
-        authDomain: "notification-de89b.firebaseapp.com",
-        projectId: "notification-de89b",
-        storageBucket: "notification-de89b.appspot.com",
-        messagingSenderId: "503530143911",
-        appId: "1:503530143911:web:39f3558bf7ee9f762f1b82",
-        measurementId: "G-P0L2HNSJEM",
-        vapidKey: "BFzZwBmO8gHoZbJDhGoqFWkNKOjofEex6vddGfTjgvYnmYwgUubBthVnCQwYyE2JsKxjc0TzPfP6gEEZh5SJ_m0"
+        apiKey: process.env["apiKey"],
+        authDomain: process.env["authDomain"],
+        projectId: process.env["projectId"],
+        storageBucket:process.env["storageBucket"],
+        messagingSenderId:process.env["messagingSenderId"],
+        appId: process.env["appId"],
+        measurementId:process.env["measurementId"],
+        vapidKey:process.env["vapidKey"]
     },
   };
--- TODOLISTBACKEND/src/firebase-messaging-sw.js
+++ TODOLISTBACKEND/src/firebase-messaging-sw.js
@@ -1,12 +1,12 @@
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 firebase.initializeApp({
-    apiKey: "AIzaSyAG3LBqWOTpnAXzbvbH-jZg_otfeAyZyv4",
-    authDomain: "notification-de89b.firebaseapp.com",
-    projectId: "notification-de89b",
-    storageBucket: "notification-de89b.appspot.com",
-    messagingSenderId: "503530143911",
-    appId: "1:503530143911:web:39f3558bf7ee9f762f1b82",
-    measurementId: "G-P0L2HNSJEM"
+    apiKey:process.env.apiKey,
+    authDomain:process.env.authDomain,
+    projectId:process.env.projectId,
+    storageBucket:process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId:process.env.appId,
+    measurementId:process.env.measurementId
 });
 const messaging = firebase.messaging();
